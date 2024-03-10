7d1c3b156482e915191d070f68d8432071bc4b9f
billc27 <110593711+billc27@users.noreply.github.com>
2024-03-10 19:11:31 +0700

    feat: login api

---
+++ app/src/main/java/com/example/transactionmanagementsystem/api/RetrofitClient.kt
@@ -0,0 +1,39 @@
+package com.example.transactionmanagementsystem.api
+
+import com.example.transactionmanagementsystem.models.LoginRequest
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface ApiInterface {
+    @Headers("Content-Type:application/json")
+    @POST("api/auth/login")
+    fun signin(@Body info: LoginRequest): retrofit2.Call<ResponseBody>
+}
+
+class RetrofitInstance {
+    companion object {
+        val BASE_URL: String = "https://pbd-backend-2024.vercel.app/"
+
+        val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client: OkHttpClient = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+
+        fun getRetrofitInstance(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
