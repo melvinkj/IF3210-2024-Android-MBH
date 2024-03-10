2896ecfe79812d8796276157d0507045f0cde790
billc27 <110593711+billc27@users.noreply.github.com>
2024-03-10 20:05:56 +0700

    feat: login feature

---
+++ app/src/main/java/com/example/transactionmanagementsystem/LoginActivity.kt
@@ -0,0 +1,54 @@
+package com.example.transactionmanagementsystem
+
+import android.widget.Toast
+import com.example.transactionmanagementsystem.api.ApiInterface
+import com.example.transactionmanagementsystem.api.RetrofitInstance
+import com.example.transactionmanagementsystem.models.LoginRequest
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val emailField = findViewById<EditText>(R.id.email)
+        val passwordField = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.login)
+
+        loginButton.setOnClickListener {
+            val email = emailField.text.toString()
+            val password = passwordField.text.toString()
+            signin(email, password)
+        }
+    }
+
+    private fun signin(email: String, password: String){
+        val retIn = RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+        val signInInfo = LoginRequest(email, password)
+
+        retIn.signin(signInInfo).enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(
+                    this@LoginActivity,
+                    t.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.code() == 200) { 
+                    Toast.makeText(this@LoginActivity, "Login success!", Toast.LENGTH_SHORT).show()
+                    println("Response Body: " + response.body()?.string())
+                } else {
+                    Toast.makeText(this@LoginActivity, "Login failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+}
