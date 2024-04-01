f1cdbf20e92e14475909b894ee5a32ccbfa2ea85
billc27 <110593711+billc27@users.noreply.github.com>
2024-04-01 20:56:58 +0700

    fix: build error

--- app/src/main/java/com/example/transactionmanagementsystem/MainActivity.kt
+++ app/src/main/java/com/example/transactionmanagementsystem/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -20,11 +12,27 @@ import androidx.fragment.app.FragmentTransaction
 import com.example.transactionmanagementsystem.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.transactionmanagementsystem.adapter.TransactionCardAdapter
+import com.example.transactionmanagementsystem.database.TransactionDatabase
+import com.example.transactionmanagementsystem.repository.TransactionRepository
+import com.example.transactionmanagementsystem.viewmodel.TransactionViewModel
+import com.example.transactionmanagementsystem.viewmodel.TransactionViewModelFactory
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : BaseActivity() {
 
 //    private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var binding : ActivityMainBinding
+
+    lateinit var transactionViewModel: TransactionViewModel
+
+    private fun setupViewModel(){
+        val transactionRepository = TransactionRepository(TransactionDatabase(this))
+        val viewModelProviderFactory = TransactionViewModelFactory(application, transactionRepository)
+        transactionViewModel = ViewModelProvider(this, viewModelProviderFactory)[TransactionViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +42,18 @@ class MainActivity : AppCompatActivity() {
             binding.navbar.setBackgroundColor(0xFFFFFFF.toInt())
         }
 
-        setContent {}
-
         setContentView(binding.root)
+        setupViewModel()
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val mainActivity = this
+//
+//        binding.recyclerview.apply {
+//            layoutManager = LinearLayoutManager(applicationContext)
+//            adapter = TransactionCardAdapter()
+//        }
+
         supportActionBar?.hide()
 //        bottomNavigationView = findViewById(R.id.navbar)
 
@@ -64,19 +81,3 @@ class MainActivity : AppCompatActivity() {
 
 
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    TransactionManagementSystemTheme {
-//        Greeting("Android")
-//    }
-//}
