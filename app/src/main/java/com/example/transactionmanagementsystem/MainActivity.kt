04a09a570d03dc3a57bf38c120b658bc7ce6cf81
Melvin Kent Jonathan <108329026+melvinkj@users.noreply.github.com>
2024-04-01 09:50:17 +0700

    fix: conflict main activity

--- app/src/main/java/com/example/transactionmanagementsystem/MainActivity.kt
+++ app/src/main/java/com/example/transactionmanagementsystem/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.transactionmanagementsystem
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,21 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.transactionmanagementsystem.adapter.TransactionCardAdapter
-import com.example.transactionmanagementsystem.database.TransactionDatabase
-import com.example.transactionmanagementsystem.databinding.ActivityMainBinding
-import com.example.transactionmanagementsystem.repository.TransactionRepository
 import com.example.transactionmanagementsystem.ui.theme.TransactionManagementSystemTheme
-import com.example.transactionmanagementsystem.viewmodel.TransactionViewModel
-import com.example.transactionmanagementsystem.viewmodel.TransactionViewModelFactory
 
-class MainActivity : BaseActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
-    lateinit var transactionViewModel: TransactionViewModel
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,23 +23,6 @@ class MainActivity : BaseActivity() {
                 }
             }
         }
-        setContentView(R.layout.activity_main)
-        setupViewModel()
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//
-//        val mainActivity = this
-//
-//        binding.recyclerview.apply {
-//            layoutManager = LinearLayoutManager(applicationContext)
-//            adapter = TransactionCardAdapter()
-//        }
-    }
-
-    private fun setupViewModel(){
-        val transactionRepository = TransactionRepository(TransactionDatabase(this))
-        val viewModelProviderFactory = TransactionViewModelFactory(application, transactionRepository)
-        transactionViewModel = ViewModelProvider(this, viewModelProviderFactory)[TransactionViewModel::class.java]
     }
 }
 
